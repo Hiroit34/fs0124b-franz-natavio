b33ee9f5a8506992e18de71cfe9cdc4c0463083b
Hiroit34 <franz.natavio@gmail.com>
2024-02-23 17:35:39 +0100

    progetto 2302

--- m2/progetto/base_di_partenza/script/script.js
+++ m2/progetto/base_di_partenza/script/script.js
@@ -1,11 +1,13 @@
-const nav = document.getElementsByTagName("nav");
+const nav = document.querySelector(".nav");
 const get = document.querySelector(".get");
 
 window.onscroll = function () {
   // pageYOffset or scrollY
-  if (window.scrollY > 0) {
+  if (window.scrollY > 399) {
     nav.classList.add("nav-white");
+    get.classList.add("get-green");
   } else {
     nav.classList.remove("nav-white");
+    get.classList.remove("get-green");
   }
 };
